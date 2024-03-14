dd254b1866bc35185739ec895bf4f8f66b88aea3
konrai <konrai630@gmail.com>
2024-03-15 07:31:37 +0900

    delete js files

--- assets/js/11.js
+++
@@ -1,1 +0,0 @@
-var json__11 = {"type":"FeatureCollection","name":"_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"プロット":"B","ID":"63","経度":135.76303,"緯度":35.07395,"樹種":"タンナサワフタギ","DBH(cm)":0.0},"geometry":{"type":"Point","coordinates":[135.7630256,35.07394723]}}]}
