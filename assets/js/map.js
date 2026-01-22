63f2b466a6eeb6db7a8d9635ba2eb3269bc23824
Maple <60655531+maple60@users.noreply.github.com>
2026-01-22 11:04:07 +0900

    migrate quarto

--- assets/js/map.js
+++
@@ -1,6 +0,0 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
-}).addTo(mymap);
